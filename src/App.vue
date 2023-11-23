49ed0319ad39d47bdb7d8f29211a2d0d5ccea612
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-23 16:54:40 +0100

    Add: array of nav bar objects

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 
+export default {
+
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {}
+  },
+
+}
 </script>
 
 <template>
-
+ <AppHeader />
 </template>
 
-<style >
-
+<style lang="scss">
+@use "./style/general.scss"
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,50 @@
-<script></script>
+<script>
 
-<template></template>
+export default {
+  data() {
+    return {
+      navItems: [
+        {
+          title: "CHARACTERS",
+        },
+        {
+          title: "COMICS",
+        },
+        {
+          title: "MOVIES",
+        },
+        {
+          title: "TV",
+        },
+        {
+          title: "GAMES",
+        },
+        {
+          title: "COLLECTIBLES",
+        },
+        {
+          title: "VIDEOS",
+        },
+        {
+          title: "FANS",
+        },
+        {
+          title: "NEWS",
+        },
+        {
+          title: "SHOP",
+        },
+      ],
+    }
+  },
+}
 
-<style scoped></style>
+</script>
+
+<template>
+  <h1>ciao</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>

d81be29a94b670f9297da15a3bd4a13c6308fdbc
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-23 17:39:21 +0100

    add: wip on border hover on list

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -42,9 +42,80 @@ export default {
 </script>
 
 <template>
-  <h1>ciao</h1>
+  <header>
+    <!-- Container -->
+    <div class="container">
+
+      <div class="wrapper flex">
+        <div class="logoCol">
+          <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+
+        <div class="linkCol">
+          <ul>
+            <li v-for="item in navItems" :key="item.title">
+              <a href="">{{ item.title }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+    <!-- Container -->
+  </header>
+  
 </template>
 
 <style lang="scss" scoped>
+@use "../style/partials/variables" as *;
+
+header {
+  
+  .wrapper {
+    height: 100px;
+
+
+    &.flex {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .logoCol {
+      
+      img {
+        width: 65px;
+      }
+
+    }
+
+    .linkCol {
+
+      ul {
+        list-style-type: none;
+        display: flex;
+
+        li {
+          font-size: .8rem;
+          padding: .5rem;
+          cursor: pointer;
+          
+          a {
+            text-decoration: none;
+            color: inherit;
+            
+          }
+
+          &:hover{
+            border-bottom: 3px solid $primary;
+          }
+        }
+
+        
+      }
+    }
+
+  }
+}
 
 </style>

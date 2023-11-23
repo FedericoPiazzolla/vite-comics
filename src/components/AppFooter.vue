8df8ca9abbec174f696252256a71d9e52dca71af
FedericoPiazzolla <Federicop257@gmail.com>
2023-11-23 19:03:19 +0100

    add: footer bottom section on AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -56,20 +56,44 @@
 
   <footer>
     <div class="footerTop">
-      <div class="footer-list">
-        <div v-for="itemList in listFooter">
-          <h4>{{ itemList.title }}</h4>
-          <ul>
-            <li v-for="item in itemList.list">
-              <a href="">{{ item }}</a>
-            </li>
-          </ul>
+      <div class="container">
+        <div class="footer-list">
+
+          <div v-for="itemList in listFooter" class="footer-list-item">
+            <h4>{{ itemList.title }}</h4>
+
+            <ul>
+              <li v-for="item in itemList.list">
+                <a href="">{{ item }}</a>
+              </li>
+            </ul>
+
+          </div>
+          
         </div>
+
+        <div class="dc-bg"></div>
+
       </div>
-      <div class="dc-bg"></div>
     </div>
 
     <div class="footerBottom">
+
+      <div class="container">
+        <button>SIGN-UP NOW!</button>
+
+        <div class="social-list">
+          <h3>FOLLOW US</h3>
+
+          <ul>
+            <li v-for="itemSocial in listSocial">
+              <a :href="`http://${itemSocial.link}`"><img :src=getImageUrl(itemSocial.icon) :alt="`Icon of ${itemSocial.icon}`"></a>
+            </li>
+          </ul>
+
+        </div>
+
+      </div>
 
     </div>
 
@@ -86,35 +110,74 @@
     background-image: url(../assets/img/footer-bg.jpg);
 
     .footerTop {
-      color: white;
-      display: flex;
-      justify-content: space-between;
-
-      .dc-bg {
-        background-position: center;
-        background-repeat: no-repeat;
-        background-image: url(../assets/img/dc-logo-bg.png);
-        width: 50%;
-        height: 400px;
-      }
-
-      .footer-list {
+      .container {
+        color: white;
         display: flex;
-        padding: 2rem 0;
-        gap: 1rem;
+        justify-content: space-between;
 
-        div {
-          padding: 1rem;
-        }
-        
-        ul {
-          list-style-type: none;
+        .dc-bg {
+          background-position: center;
+          background-repeat: no-repeat;
+          background-image: url(../assets/img/dc-logo-bg.png);
+          background-size: cover;
+          width: 50%;
+          height: 400px;
+          align-self: flex-end;
         }
 
-        a {
-          color: gray;
-          font-size: .7rem;
-          text-decoration: none;
+        .footer-list {
+          display: flex;
+          gap: 1rem ;
+
+          .footer-list-item {
+            padding: 1rem 0;
+
+            ul {
+              list-style-type: none;
+            }
+
+            a {
+              color: gray;
+              font-size: .7rem;
+              text-decoration: none;
+            }
+          }
+        }
+      }
+    }
+
+    .footerBottom {
+      height: 100px;
+      background-color: $bg_footer_dark;
+
+      .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+
+        button {
+          border: 2px solid $primary;
+          padding: .8rem 1rem;
+          background-color: $bg_footer_dark;
+          color: white;
+          font-weight: bolder;
+        }
+
+        .social-list {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          color: $primary;
+            
+          ul {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            list-style-type: none;
+            gap: 1rem;
+            margin-left: 1rem;
+          }
         }
       }
     }
